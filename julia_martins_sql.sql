1dea239db9f0dc85059809c1a03dc0879fc99b58
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-10-15 08:17:03 -0300

    feat: delete 41 ao 50 prontos

--- julia_martins_sql.sql
+++ julia_martins_sql.sql
@@ -8,5 +8,15 @@ DELETE FROM produtos WHERE preco > 45.00;
 DELETE FROM produtos WHERE nome = 'Pasta Sanfonada';
 DELETE FROM produtos WHERE categoria = 'Arte e Desenho' AND preco > 25.00;
 DELETE FROM produtos WHERE preco BETWEEN 20.00 AND 30.00;
+DELETE FROM produtos WHERE nome LIKE '%Lapis%';
+DELETE FROM produtos WHERE categoria = 'Papelaria Criativa' AND preco < 15.00;
+DELETE FROM produtos WHERE id BETWEEN 20 AND 25;
+DELETE FROM produtos WHERE preco < 15.00;
+DELETE FROM produtos WHERE nome = 'Marcador de Texto Amarelo';
+DELETE FROM produtos WHERE categoria = 'Material Escolar' AND preco > 40.00;
+DELETE FROM produtos WHERE id > 190;
+DELETE FROM produtos WHERE categoria LIKE '%O%';
+DELETE FROM produtos WHERE categoria = 'Escritorio' AND preco > 35.0;
+DELETE FROM produtos WHERE nome LIKE '%F%';
 
 
